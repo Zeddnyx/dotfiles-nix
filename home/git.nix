d978ac3f0b556a8836c4573a86484d55766f4dbf
Jee <zeddnyx08@gmail,com>
2025-02-24 10:56:25 +0700

    refactor(git): adding more config

--- home/git.nix
+++ home/git.nix
@@ -5,10 +5,21 @@
     enable = true;
     userName = "Jee";
     userEmail = "jee.nvim@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+      color.ui = "auto";
+      push = {
+        default = "current";
+      };
+      merge = {
+        conflictstyle = "diff3";
+        stat = "true";
+      };
+    };
     aliases = {
       g = "git";
       ga = "git add";
-      gc = "git commit -m";
+      gc = "!f() { git commit -m \"$1\"; }; f";
       gs = "git status --short";
       gpl = "git pull origin";
       gck = "git checkout";
@@ -16,5 +27,15 @@
       gpush = "git push origin";
       gpull = "git pull origin";
     };
+    ignores = [
+      "*~"
+      "*.swp"
+      "*result*"
+      ".direnv"
+      ".env"
+      ".env.local"
+      ".next"
+      "node_modules"
+    ];
   };
 }
